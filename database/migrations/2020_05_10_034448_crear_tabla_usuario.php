fe8d62ea59b980b0053976f0d41f2f06a20b1a07
ggarciaf79 <germangarciafiloth@gmail.com>
2020-05-10 00:05:44 -0500

    Se crearon los archivos de migracion

--- database/migrations/2020_05_10_034448_crear_tabla_usuario.php
+++ database/migrations/2020_05_10_034448_crear_tabla_usuario.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CrearTablaUsuario extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('usuario',50);
+            $table->string('password',100);
+            $table->string('nombre',50);
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuario');
     }
 }
---
+++ database/migrations/2020_05_10_044925_crear_tabla_libro_prestamo.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrearTablaLibroPrestamo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libro_prestamo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('usuario_id');
+            $table->foreign('usuario_id', 'fk_libropretamo_usuario')->references('id')->on('usuario')->ondelete('restrict')->onupdate('restrict');
+            $table->unsignedInteger('libro_id');
+            $table->foreign('libro_id', 'fk_libropretamo_libro')->references('id')->on('libro')->ondelete('restrict')->onupdate('restrict');
+            $table->date('fecha_prestamo');
+            $table->string('prestamo_a',100);
+            $table->date('fecha_devolucion')->nullable();
+            $table->boolean('estado');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libro_prestamo');
+    }
+}
